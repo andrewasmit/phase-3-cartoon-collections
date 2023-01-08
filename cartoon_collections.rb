0c2004816d6957bce3c74bb21c3f56a5f6e46cf4
andrew <andrewasmit@hotmail.com>
2023-01-07 22:06:22 -0600

    commit tree 3070b51aa5dc7b616c418f148aa780d2ded48cce

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves arr
+  arr.each_with_index {|d, idx| puts "#{idx +1}. #{d}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet arr
+  arr.map { |w| w.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls arr
+  arr.any? {|w| w.length > 4}
 end
 
-def find_the_cheese# code an argument here
+
+def find_the_cheese arr
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  newArr = arr.filter { |f| cheese_types.include?(f)}
+  newArr[0];
 end
